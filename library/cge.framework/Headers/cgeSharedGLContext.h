564508b7eaa03e31e85536bd16170151e1ec21fe
Yang Wang <admin@wysaid.org>
2017-06-12 14:37:13 +0800

    update

--- library/cge.framework/Headers/cgeSharedGLContext.h
+++ library/cge.framework/Headers/cgeSharedGLContext.h
@@ -11,7 +11,7 @@
 
 #import <Foundation/Foundation.h>
 #import <OpenGLES/EAGL.h>
-#import "cgeProcessingContext.h"
+#import <cge/cgeProcessingContext.h>
 
 @interface CGESharedGLContext : CGEProcessingContext
 
