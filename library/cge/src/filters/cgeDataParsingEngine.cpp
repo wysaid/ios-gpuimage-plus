38a284f2878cccfd4548540a27bc2b349fc88763
Yang Wang <admin@wysaid.org>
2017-06-22 11:50:38 +0800

    some fix

--- library/cge/src/filters/cgeDataParsingEngine.cpp
+++ library/cge/src/filters/cgeDataParsingEngine.cpp
@@ -150,6 +150,7 @@ tableParserHelper(vec, pstr + n, i - n); \
 		if(vecRGB.empty() && vecR.empty() && vecG.empty() && vecB.empty())
 		{
 			CGE_LOG_ERROR("curveParser - Empty Curve!!\n");
+			delete proc;
 			return nullptr;
 		}
 
@@ -181,6 +182,7 @@ tableParserHelper(vec, pstr + n, i - n); \
 			if(!proc->init())
 			{
 				CGE_LOG_ERROR("CGEDataParsingEngine::lomoWithCurveParser Create filter Failed!\n");
+				delete proc;
 				return nullptr;
 			}
 			CGE_LOG_INFO("lomoWithCurveParser - Curve With Texture is used!(Not error, everything is ok)\n");
@@ -1218,6 +1220,11 @@ do{\
 				filter->setDistanceNormalizationFactor(disFactor);
 				filter->setRepeatTimes(repeatTimes);
 			}
+			else
+			{
+				LOG_ERROR_PARAM(pstr);
+				delete filter;
+			}
 			
 		}
         else if(strcmp(buffer, "face") == 0)
@@ -1353,6 +1360,7 @@ do{\
                     break;
                 default:
                     CGE_LOG_ERROR("Error which should never happen, but just happened... biu biu...\n");
+                    delete filter;
                     break;
                 }
 			}
