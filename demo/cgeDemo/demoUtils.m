db8a7656df1d26fa096f6408e582f1336ce8307e
Yang Wang <admin@wysaid.org>
2017-12-09 18:40:54 +0800

    two new cases

--- demo/cgeDemo/demoUtils.m
+++ demo/cgeDemo/demoUtils.m
@@ -13,6 +13,8 @@
 const char* g_effectConfig[] = {
     nil,
     "@curve RGB(0,255)(255,0) @style cm mapping0.jpg 80 80 8 3", // ASCII art (字符画效果)
+    "@dynamic motionflow 12 0",
+    "@dynamic soulstuff 240 320",
     "@adjust lut edgy_amber.png",
     "@adjust lut filmstock.png",
     "@adjust lut foggy_night.png",
